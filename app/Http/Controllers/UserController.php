bf2b47d101c2835f6f49b2d7b67d9805ae1f2938
ggustavo-souza <cavalogames1231@gmail.com>
2025-11-27 21:19:16 -0300

    Traduzindo e implementando o editar senha.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -16,10 +17,10 @@ class UserController extends Controller
     {
         // Busca todos os usuários, 10 por página
         $users = User::paginate(10);
-        
+
         return view('users.index', compact('users'));
     }
- public function edit(User $user)
+    public function edit(User $user)
     {
         return view('users.edit', compact('user'));
     }
@@ -33,7 +34,14 @@ class UserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', Rule::unique('users')->ignore($user->id)],
             'is_admin' => ['required', 'boolean'],
+            'password' => ['nullable', 'string', 'min:8'],
         ]);
+
+        if ($request->filled('password')) {
+            $validated['password'] = Hash::make($validated['password']);
+        } else {
+            unset($validated['password']);
+        }
 
         $user->update($validated);
 
